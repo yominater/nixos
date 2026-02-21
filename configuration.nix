1e8ed66f70705c42f165d85e816a4714c118bace
yominater <alexandertrains4@gmail.com>
2026-02-20 19:31:03 -0500

    37 25.11.5960.3aadb7ca9eac

--- configuration.nix
+++ configuration.nix
@@ -100,7 +100,7 @@ in
   services.xserver.enable = true;
 
   # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.enable = false;
   services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
