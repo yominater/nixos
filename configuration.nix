16f622a432b6a9a1e3d5049adf71d59c07252863
yominater <alexandertrains4@gmail.com>
2026-02-21 12:27:40 -0500

    64 25.11.6327.c217913993d6

--- configuration.nix
+++ configuration.nix
@@ -173,11 +173,23 @@ in
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC="performance";
       CPU_SCALING_GOVERNOR_ON_BAT="powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC="performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT="power";
+
       CPU_SCALING_MIN_FREQ_ON_BAT="1400000";
       CPU_SCALING_MAX_FREQ_ON_BAT="1600000";
       CPU_SCALING_MIN_FREQ_ON_AC="1600000";
       CPU_SCALING_MAX_FREQ_ON_AC="1400000";
+      TLP_DEFAULT_MODE="bat";
+      
+      CPU_BOOST_ON_AC="1";
+      CPU_BOOST_ON_BAT="0";
 
+      PLATFORM_PROFILE_ON_AC="performance";
+      PLATFORM_PROFILE_ON_BAT="low-power";
+
+      START_CHARGE_THRESH_BAT0="50";
+      STOP_CHARGE_THRESH_BAT0="80";
     };
   };
 
