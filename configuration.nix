ae9967a8c9fcf49817b60feef5e198b2645a032c
yominater <alexandertrains4@gmail.com>
2026-04-26 16:03:56 -0400

    10 25.11.9586.10e7ad5bbcb4

--- configuration.nix
+++ configuration.nix
@@ -226,6 +226,8 @@ in
   enable32Bit = true;
   };
 
+  zramSwap.enable = true;
+
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
