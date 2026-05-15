e405aa41225fec7c87c4c57c0900fe1726ff6669
yominater <alexandertrains4@gmail.com>
2026-05-15 15:14:49 -0400

    23 25.11.10830.d7a713c0b7e4

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,7 @@ in
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_7_0;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
